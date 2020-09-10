734e8c94a1ccfb1caefb46471f0def5969c4e2ab
dongha1992 <58624851+dongha1992@users.noreply.github.com>
2020-09-10 15:26:28 +0900

    v2

--- js/index.js
+++ js/index.js
@@ -1,7 +1,27 @@
+const body = document.querySelector("body")
+const closeBtn = document.querySelector(".close")
+const bg = document.querySelector(".bg")
+const popup = document.querySelector(".popup")
 const header = document.querySelector(".header")
 const li = document.getElementById("list");
 const toggleBtn = document.querySelector(".toggle");
-const img = document.querySelectorAll("img")
+const logo = document.querySelector(".logo");
+
+
+
+function showCard() {
+
+    popup.style.display = "block"
+    bg.style.display = "block"
+    body.classList.add("hidden")
+
+}
+
+function closeCard() {
+    popup.style.display = "none"
+    bg.style.display = "none"
+    body.classList.remove("hidden")
+}
 
 function handleScroll() {
     header.classList.toggle("sticky", window.scrollY > 0)
@@ -10,6 +30,9 @@ function handleScroll() {
 
 function init() {
     window.addEventListener("scroll", handleScroll);
+    closeBtn.addEventListener("click", closeCard)
+
+
 }
 
 init()
